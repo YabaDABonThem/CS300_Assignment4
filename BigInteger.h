22a6ccbf80c68c55ef019fd0e32ce81ddbfe17f7
Allen Bao <allenbao@uw.edu>
2022-11-13 21:50:32 -0800

    addition and positive subtraction shell done

--- BigInteger.h
+++ BigInteger.h
@@ -7,11 +7,13 @@
 class BigInteger : public DoublyLinkedList<int> {
     
 protected:
-    bool isNegative = false;
+    bool isPositive = true;
 
 public:
-    BigInteger() {
-
+    BigInteger(String num) {
+        if (charAt(0) == '-') {
+            isPositive = false;
+        }
     }
 
     ~BigInteger() {
@@ -26,39 +28,78 @@ public:
         BigInteger sum;
         int carryOver = 0; // represents whether or not there was a carry over digit (being 1)
         int columnSum = 0; // represents the sum of all items in the column
-        while (true) { // we do different things depending on whether leftNumIter or rightNumIter is null
-            if (leftNumIter && rightNumIter) {
-                columnSum = leftNumIter->data + rightNumIter->data + carryOver;
-                carryOver = 0; // set the carryOver to 0 after you use it
-                sum.insertFirst(columnSum % 10); // insert the number in the one's place
-                if (columnSum >= 10) {
-                    carryOver = 1;
-                }
-            } else if (leftNumIter) { // If left number is longer than the right
+        while (leftNumIter || rightNumIter) { // while there are numbers left, keep adding
+            if (!rightNumIter) {
                 columnSum = leftNumIter->data + carryOver;
-                carryOver = 0; // set the carryOver to 0 after you use it
-                sum.insertFirst(columnSum % 10); // insert the number in the one's place
-                if (columnSum >= 10) {
-                    carryOver = 1; // need this check bc 9 + carry over 1 is still possible
-                }
-            } else if (rightNumIter) { // If right number is longer than the left
+            } else if (!leftNumIter) {
                 columnSum = rightNumIter->data + carryOver;
-                carryOver = 0; // set the carryOver to 0 after you use it
-                sum.insertFirst(columnSum % 10); // insert the number in the one's place
-                if (columnSum >= 10) {
-                    carryOver = 1;
-                }
             } else {
-                return sum;
+                columnSum = leftNumIter->data + rightNumIter->data + carryOver;
             }
+            carryOver = 0; // set the carryOver to 0 after you use it
+            sum.insertFirst(columnSum % 10); // insert the number in the one's place
+            if (columnSum >= 10) {
+                carryOver = 1;
+            }
+            leftNumIter = leftNumIter->prev;
+            rightNumIter = rightNumIter->prev;
         }
+        return sum;
     }
 
     // How to implement subtraction? (How do negative numbers work?)
     // the tests expect you to support negative numbers
     typedef Node<int> IntNode;
     BigInteger BigInteger::operator- (const BigInteger& rightNum) const {
-        
+        // You must first decide which number is larger and determine which number subtracted by the other based off that
+        IntNode *leftNumIter = this->last; 
+        IntNode *rightNumIter = rightNum.last;
+        BigInteger difference;
+        int borrowedDigit = 0; // represents whether or not there was a borrowed 1 from the previous digit (being 10)
+        int columnDiff = 0; 
+        while (leftNumIter || rightNumIter) {
+            // first scenario: being to subtract normally
+            if (leftNumIter->data >= rightNumIter->data) {
+                columnDiff = leftNumIter->data + borrowedDigit - rightNumIter->data;
+                borrowedDigit = 0;
+                // Note: there is no need to check if you need to change the borrowed digit or not
+                difference.insertFirst(columnDiff);
+            }
+
+            // second scenario: need to borrow a digit from the next avilable number
+        }
+        return difference;
     }
+
+    bool BigInteger::operator> (const BigInteger& rightNum) const {
+        // is there a difference between a positive and a negative zero?
+        // scenario: they have different signs
+        if (this->isPositive != rightNum.isPositive) {
+            if (this->isPositive) {
+                return true; 
+            }
+            return false;
+        }
+        // scenario: they have he same signs
+        bool isLeftAbsBigger = this->isPositive; // we compare the absolute value, if they have the same sign then
+        if (this->length > rightNum.length) { // the absolute value is larger
+            return isLeftAbsBigger;
+        } else if (rightNum.length > this->length) {
+            return !isLeftAbsBigger;
+        } else { // numbers are the same length, we need to iterate through both linked lists
+            IntNode *leftNumIter = this->first; 
+            IntNode *rightNumIter = rightNum.first;
+            while (leftNumIter) {
+                if (leftNumIter->data > rightNumIter->data) {
+                    return isLeftAbsBigger; // left number is absolutely bigger
+                } else if (leftNumIter->data < rightNumIter->data) {
+                    return !isLeftAbsBigger; // right number is absolutely bigger
+                }
+                leftNumIter = leftNumIter->next;
+                rightNumIter = rightNumIter->next;
+            }
+            return false; // they are equal to each other
+        }
+    } 
     
 };
